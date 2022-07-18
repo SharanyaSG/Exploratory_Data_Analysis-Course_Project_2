967aa7d35888b6e4c46f5a47ea7d74649689e2f8
Sharanya Gowda <gowda.sharanya310@gmail.com>
2022-07-18 01:22:54 -0400

    New script and plots

--- plot2.R
+++ plot2.R
@@ -6,14 +6,14 @@
   sum_SSC <- readRDS("summarySCC_PM25.rds")
 
 ##-----------------------------------------------------------------------------------------------------------------##
-## PROJECT QUESTION 1 - Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
-## Using the base plotting system, make a plot showing the total PM2.5 emission from all sources for each of the 
-## years 1999, 2002, 2005, and 2008.
+## PROJECT QUESTION 2 - Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510"
+##fips == "24510") from 1999 to 2008? Use the base plotting system to make a plot answering this question.
 ##-----------------------------------------------------------------------------------------------------------------##
-  emi_total<-tapply(sum_SSC$Emissions, sum_SSC$year, sum, na.rm=T)
+  sum_SSC_PMdec<-subset(sum_SSC, fips == "24510")
+  MD_annual<-tapply(sum_SSC_PMdec$Emissions, sum_SSC_PMdec$year, sum, na.rm=TRUE)
   par(mfrow=c(1,1),mar=c(7,7,4,3))
-  barplot(emi_total/10000, names.arg = names(emi_total), col="yellow", main="ANNUAL PM2.5 EMISSIONS", 
-          xlab = "Year", ylab="PM2.5 Emissions in Kilotons", ylim = range(0,emi_total/10000) * 1.3)
-  lines(names(emi_total), emi_total/10000,col="yellow") 
-  png("plot1.png", width=500, height=500)
+  barplot(MD_annual, names.arg = names(MD_annual), col="green", 
+        main="ANNUAL PM2.5 EMISSIONS (Baltimore City,MD)", xlab = "Year", 
+        ylab="PM2.5 Emissions in tons", ylim = range(0,MD_annual)*1.3) 
+  png("plot2.png", width=640, height=640)
   dev.off()
